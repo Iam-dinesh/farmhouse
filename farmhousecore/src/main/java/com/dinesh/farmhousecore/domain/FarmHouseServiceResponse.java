a40ffac46b9dfdd61b867b6348926ed55f65d81f
Iam-dinesh <dineshraju964@gmail.com>
2024-09-15 22:31:21 +0530

    project setup

--- farmhousecore/src/main/java/com/dinesh/farmhousecore/domain/FarmHouseServiceResponse.java
+++ farmhousecore/src/main/java/com/dinesh/farmhousecore/domain/FarmHouseServiceResponse.java
@@ -1,22 +1,19 @@
 package com.dinesh.farmhousecore.domain;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class FarmHouseServiceResponse {
+import lombok.Data;
+
+@Data
+public class FarmHouseServiceResponse implements Serializable {
+
+	private static final long serialVersionUID = 7931575631889750413L;
 
 	private List<String> message;
 	private StatusCode statusCode;
+
 	public List<String> getMessage() {
 		return message;
 	}
-	public void setMessage(List<String> message) {
-		this.message = message;
-	}
-	public StatusCode getStatusCode() {
-		return statusCode;
-	}
-	public void setStatusCode(StatusCode statusCode) {
-		this.statusCode = statusCode;
-	}
-
 }
--- farmhousecore/src/main/java/com/dinesh/farmhousecore/domain/OwnerDetails.java
+++ farmhousecore/src/main/java/com/dinesh/farmhousecore/domain/OwnerDetails.java
@@ -3,6 +3,9 @@ package com.dinesh.farmhousecore.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import lombok.Data;
+
+@Data
 public class OwnerDetails implements Serializable {
 
 	private static final long serialVersionUID = -222865914633308943L;
@@ -12,45 +15,5 @@ public class OwnerDetails implements Serializable {
 	private int ownerAddressID;
 	private Date dateCreated = new Date();
 	private Date dateModified;
-
-	public int getOwnerID() {
-		return ownerID;
-	}
-
-	public void setOwnerID(int ownerID) {
-		this.ownerID = ownerID;
-	}
-
-	public String getOwnerName() {
-		return ownerName;
-	}
-
-	public void setOwnerName(String ownerName) {
-		this.ownerName = ownerName;
-	}
-
-	public int getOwnerAddressID() {
-		return ownerAddressID;
-	}
-
-	public void setOwnerAddressID(int ownerAddressID) {
-		this.ownerAddressID = ownerAddressID;
-	}
-
-	public Date getDateCreated() {
-		return dateCreated;
-	}
-
-	public void setDateCreated(Date dateCreated) {
-		this.dateCreated = dateCreated;
-	}
-
-	public Date getDateModified() {
-		return dateModified;
-	}
-
-	public void setDateModified(Date dateModified) {
-		this.dateModified = dateModified;
-	}
 
 }
