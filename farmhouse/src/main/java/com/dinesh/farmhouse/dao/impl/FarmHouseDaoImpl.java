2be4f52cea51dc9b16e6a7725ecba502c81a0f1e
Iam-dinesh <dineshraju964@gmail.com>
2024-09-16 22:56:41 +0530

    dao impl

--- farmhouse/src/main/java/com/dinesh/farmhouse/dao/impl/FarmHouseDaoImpl.java
+++ farmhouse/src/main/java/com/dinesh/farmhouse/dao/impl/FarmHouseDaoImpl.java
@@ -1,5 +1,7 @@
 package com.dinesh.farmhouse.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.dinesh.farmhouse.dao.FarmHouseDao;
@@ -7,6 +9,9 @@ import com.dinesh.farmhousecore.domain.FarmHouse;
 
 @Repository
 public class FarmHouseDaoImpl implements FarmHouseDao {
+
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplate;
 
 	@Override
 	public boolean createFarmHouse(FarmHouse farmHouse) {
